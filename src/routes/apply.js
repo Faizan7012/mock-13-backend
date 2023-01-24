6cb52e0cd0a249a23de01ca07c866ce7427c5c92
Faizan7012 <frtoons7012@gmail.com>
2023-01-24 14:58:34 +0530

    new

--- src/routes/apply.js
+++ src/routes/apply.js
@@ -3,7 +3,6 @@ const { applyJob, getAppJob } = require('../controller/apply');
 const { userMiddle } = require('../middleware/user');
 const Ar = express.Router();
 
-Ar.use(userMiddle)
 Ar.get('/',getAppJob)
 Ar.post('/:id', applyJob)
 
